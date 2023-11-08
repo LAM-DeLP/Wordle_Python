241335c22970ca552c9c8858a9c3c1281b7c08b5
LAM-DeLP <de.lamnp42@gmail.com>
2023-11-08 10:15:37 +0900

    clean code

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ answ = ["Guide","sence","Orbit"]
 def ext_char(input_list):
     return [str(char) for char in input_list]
 
-answ_char = ext_char(random.choice(answ))
-input_char = ext_char(input())
-
 def create_matrix(list1,list2):
     crspnd_list = [[j for j in range(len(list1)) if list1[i]==list2[j]] for i in range(len(list1))]
-    
     return crspnd_list
+
+answ_char = ext_char(random.choice(answ))
+input_char = ext_char(input())
 
 print(answ_char)
 print(input_char)
