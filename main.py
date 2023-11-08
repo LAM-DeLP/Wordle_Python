ca40e1c07d496c3f10e69c769713cfce30b9a604
LAM-DeLP <de.lamnp42@gmail.com>
2023-11-08 09:53:16 +0900

    add function to create matrix

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ def ext_char(input_list):
 answ_char = ext_char(random.choice(answ))
 input_char = ext_char(input())
 
-def check_crrspnd(list1,list2):
-    crspnd_list = [int(i) for i in range(len(list1)) if list1[i]==list2[i]]
-    print(crspnd_list)
+def create_matrix(list1,list2):
+    crspnd_list = [[j for j in range(len(list1)) if list1[i]==list2[j]] for i in range(len(list1))]
+    
+    return crspnd_list
 
 
 print(answ_char)
 print(input_char)
-check_crrspnd(input_char,answ_char)
+print(create_matrix(input_char,answ_char))
