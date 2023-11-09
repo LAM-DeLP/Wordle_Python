6ff45ba635ae3334c8eb993e14eabc691f07013a
LAM-DeLP <de.lamnp42@gmail.com>
2023-11-10 00:43:32 +0900

    some fix

--- config/datamanager.py
+++ config/datamanager.py
@@ -1,15 +1,19 @@
 import csv
-import re
 
-tempList = []
-data = []
-with open("wordle_python\\config\\downloadList.csv","r",encoding="UTF-8") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        tempList.append(row[3])
-    data = [[tempList[i]] for i in range(len(tempList))]
+class datamaneger:
+    
+    def __init__(self):
+        data = []
+        with open("wordle_python\\config\\downloadList.csv","r",encoding="UTF-8") as f:
+            reader = csv.reader(f)
+            data = [[row[3]] for row in reader]
 
-with open("wordle_python\\config\\vocabList.csv","w",encoding="UTF-8",newline="") as f:
-    writer = csv.writer(f)
-    writer.writerows(data)
+        with open("wordle_python\\config\\vocabList.csv","w",encoding="UTF-8",newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
+    
+    
+
+manager = datamaneger()
+
 
