957c02baafd9edaace003b673c2e9e0bc80bbd19
LAM-DeLP <de.lamnp42@gmail.com>
2023-11-10 01:47:31 +0900

    add function

--- config/datamanager.py
+++ config/datamanager.py
@@ -1,19 +1,28 @@
 import csv
+import random
 
 class datamaneger:
     
+    vocabfile = "wordle_python\\config\\vocabList.csv"
+
     def __init__(self):
         data = []
+        self.word = ""
         with open("wordle_python\\config\\downloadList.csv","r",encoding="UTF-8") as f:
             reader = csv.reader(f)
             data = [[row[3]] for row in reader]
 
-        with open("wordle_python\\config\\vocabList.csv","w",encoding="UTF-8",newline="") as f:
+        with open(datamaneger.vocabfile,"w",encoding="UTF-8",newline="") as f:
             writer = csv.writer(f)
             writer.writerows(data)
-    
-    
+
+    def choiceWord(self):
+        with open(datamaneger.vocabfile,"r",encoding="UTF-8") as f:
+            lines = f.readlines()
+            self.word = lines[(random.randint(0,sum(1 for line in lines)))]
+
 
 manager = datamaneger()
-
+manager.choiceWord()
+print(manager.word)
 
